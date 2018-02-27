fd5a4b40fec9bf70d70d7cd38c03cb26ce924a1a
Desolatedesu <desolatedesu@yandex.ru>
2018-02-27 23:46:49 +0300

    nvm

--- cgi-bin/engine.py
+++ cgi-bin/engine.py
@@ -6,36 +6,36 @@ cgitb.enable()
 sys.stderr = sys.stdout
 
 print('''\
-	Content-type: text/html\r\n
-	<html>
-      <head>
-          <meta charset="UTF-8">
-          <meta name="viewport" content="width=device-width, initial-scale=1.0">
-          <meta http-equiv="X-UA-Compatible" content="ie=edge">
-          <title>Desu Page</title>
-          <link rel="shortcut icon" href="/3.png" type="image/png">
-          <link rel="stylesheet" href="https://unpkg.com/normalize.css@8.0.0/normalize.css">
-          <link rel="stylesheet" href="./css/index.css">
-          <link href="https://fonts.googleapis.com/css?family=Fira+Sans+Extra+Condensed:400,400i,700,700i&amp;subset=cyrillic" rel="stylesheet">
-      </head>
-      <body>
+Content-type: text/html\r\n
+<html>
+	<head>
+		<meta charset="UTF-8">
+		<meta name="viewport" content="width=device-width, initial-scale=1.0">
+		<meta http-equiv="X-UA-Compatible" content="ie=edge">
+		<title>Desu Page</title>
+		<link rel="shortcut icon" href="/3.png" type="image/png">
+		<link rel="stylesheet" href="https://unpkg.com/normalize.css@8.0.0/normalize.css">
+		<link rel="stylesheet" href="./css/index.css">
+		<link href="https://fonts.googleapis.com/css?family=Fira+Sans+Extra+Condensed:400,400i,700,700i&amp;subset=cyrillic" rel="stylesheet">
+	</head>
+	<body>
 	''')
 
-with open('../public/header.html', mode='r', encoding='utf-8', errors=None) as file_read:
+with open('../public_html/header.html', mode='r', encoding='utf-8', errors=None) as file_read:
 	for line in file_read:
 		print(line)
 
 print('''
-	<div class='main-piska'>
+	<div class='wrapper body-wrapper'>
 	''')
 
-with open('../public/main-content.html', mode='r', encoding='utf-8', errors=None) as file_read:
+with open('../public/main.html', mode='r', encoding='utf-8', errors=None) as file_read:
 	for line in file_read:
 		print(line)
 
-with open('../public/aside.html', mode='r', encoding='utf-8', errors=None) as file_read:
-	for line in file_read:
-		print(line)
+print('''
+	</div>
+	''')
 
 with open('../public/footer.html', mode='r', encoding='utf-8', errors=None) as file_read:
 	for line in file_read:
